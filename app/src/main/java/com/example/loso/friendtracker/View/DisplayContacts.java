b6592eb9b05947d8aea469246e4ae9a0e9aebda2
rmit-s3579161-loso-lai <s3579161@student.rmit.edu.au>
2017-08-20 17:20:41 +1000

    Adjust activity_display_contacts.xml

--- app/src/main/java/com/example/loso/friendtracker/View/DisplayContacts.java
+++ app/src/main/java/com/example/loso/friendtracker/View/DisplayContacts.java
@@ -2,6 +2,8 @@ package com.example.loso.friendtracker.View;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.loso.friendtracker.R;
 
@@ -11,5 +13,10 @@ public class DisplayContacts extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_contacts);
+
+        String[] items = {"AAA", "BBB", "CCC", "DDD", "EEE", "FFF"};
+        ListView contactList = (ListView)findViewById(R.id.lvContactsList);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        contactList.setAdapter(adapter);
     }
 }
