67125d6ba73c1ff3bf0cff743b75289efe43c373
rmit-s3579161-loso-lai <s3579161@student.rmit.edu.au>
2017-10-08 10:06:24 +1100

    each action need to clean notification

--- app/src/main/java/com/example/loso/friendtracker/Alarm/ActionCancelSuggestionActivity.java
+++ app/src/main/java/com/example/loso/friendtracker/Alarm/ActionCancelSuggestionActivity.java
@@ -2,6 +2,7 @@ package com.example.loso.friendtracker.Alarm;
 
 import android.app.Activity;
 import android.app.AlarmManager;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
@@ -19,6 +20,8 @@ public class ActionCancelSuggestionActivity extends Activity {
         PendingIntent pendingIntent = PendingIntent.getBroadcast(this, AlarmSuggestionReceiver.ALARM_SUGGESTION_ID, intent, 0);
         AlarmManager alarmManager = (AlarmManager)this.getSystemService(Context.ALARM_SERVICE);
         alarmManager.cancel(pendingIntent);
+        NotificationManager notificationManager = (NotificationManager)getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.cancel(AlarmSuggestionReceiver.ALARM_SUGGESTION_ID);
         finish(); // since finish() is called in onCreate(), onDestroy() will be called immediately
     }
 }
